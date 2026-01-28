27062e098ae79a584e144b210c9bd65fc67c1d0b
egorzotov09 <egor.zotov09@bk.ru>
2026-01-28 21:33:17 +0500

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,10 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+m = numbers.index(None)
+b = []
+for num in numbers:
+    if num is not None:
+        b.append(num)
+total_sum = sum(b)
+a = total_sum /( len(b)+ 1 )
+numbers [m] = a
+print("Измененный список:",numbers)
